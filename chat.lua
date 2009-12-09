c8ee6390a21a16a014e409986ab5f394abf8756a
Trond A Ekseth <troeks@gmail.com>
2009-12-09 12:28:54 +0100

    party leader meh

--- chat.lua
+++ chat.lua
@@ -52,6 +52,7 @@ local blacklist = {
 _G.CHAT_GUILD_GET = '|Hchannel:Guild|hg|h %s:\32'
 _G.CHAT_RAID_GET = "|Hchannel:raid|hr|h %s:\32"
 _G.CHAT_PARTY_GET = "|Hchannel:Party|hp|h %s:\32"
+_G.CHAT_PARTY_LEADER_GET = '|Hchannel:party|hP|h %s:\32'
 _G.CHAT_RAID_WARNING_GET = "w %s:\32"
 _G.CHAT_RAID_LEADER_GET = "|Hchannel:raid|hR|h %s:\32"
 _G.CHAT_OFFICER_GET = "|Hchannel:o|ho|h %s:\32"
