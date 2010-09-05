55af7a1c5c0953439bc9f2a090012681f8dbadc5
Trond A Ekseth <troeks@gmail.com>
2010-09-05 15:54:46 +0200

    Remove the setfenv trick as it won't work with CC.

--- globals.lua
+++ globals.lua
@@ -1,21 +1,14 @@
-local _ENV_metatable = { __index = _G }
-local _ENV = setmetatable({
-	_G = setmetatable({
-		CHAT_SAY_GET = 's %s:\32',
-		CHAT_GUILD_GET = '|Hchannel:Guild|hg|h %s:\32',
-		CHAT_RAID_GET = "|Hchannel:raid|hr|h %s:\32",
-		CHAT_PARTY_GET = "|Hchannel:Party|hp|h %s:\32",
-		CHAT_PARTY_LEADER_GET = '|Hchannel:party|hP|h %s:\32',
-		CHAT_PARTY_GUIDE_GET = '|Hchannel:party|hP|h %s:\32',
-		CHAT_RAID_WARNING_GET = "w %s:\32",
-		CHAT_RAID_LEADER_GET = "|Hchannel:raid|hR|h %s:\32",
-		CHAT_OFFICER_GET = "|Hchannel:o|ho|h %s:\32",
-		CHAT_BATTLEGROUND_GET = "|Hchannel:Battleground|hb|h %s:\32",
-		CHAT_BATTLEGROUND_LEADER_GET = "|Hchannel:Battleground|hB|h %s:\32",
-	}, _ENV_metatable)
-}, _ENV_metatable)
-
-setfenv(ChatFrame_MessageEventHandler, _ENV)
+_G.CHAT_SAY_GET = 's %s:\32'
+_G.CHAT_GUILD_GET = '|Hchannel:Guild|hg|h %s:\32'
+_G.CHAT_RAID_GET = "|Hchannel:raid|hr|h %s:\32"
+_G.CHAT_PARTY_GET = "|Hchannel:Party|hp|h %s:\32"
+_G.CHAT_PARTY_LEADER_GET = '|Hchannel:party|hP|h %s:\32'
+_G.CHAT_PARTY_GUIDE_GET = '|Hchannel:party|hP|h %s:\32'
+_G.CHAT_RAID_WARNING_GET = "w %s:\32"
+_G.CHAT_RAID_LEADER_GET = "|Hchannel:raid|hR|h %s:\32"
+_G.CHAT_OFFICER_GET = "|Hchannel:o|ho|h %s:\32"
+_G.CHAT_BATTLEGROUND_GET = "|Hchannel:Battleground|hb|h %s:\32"
+_G.CHAT_BATTLEGROUND_LEADER_GET = "|Hchannel:Battleground|hB|h %s:\32"
 
 ChatTypeInfo['SAY'].sticky = 1
 ChatTypeInfo['YELL'].sticky = 1
